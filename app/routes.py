56e4a36341d7f32a252888e6495cda273e0188a6
Shankar <123456789987654321sr@gmail.com>
2020-01-09 21:48:49 +0530

    added form

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 from flask import render_template
 from flask import Response
 from .app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def home_page():
-    sampleuser = {'username':'Shankar'}
-    posts = [
+sampleuser = {'username':'Shankar'}
+posts = [
         {
             'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
@@ -16,5 +14,15 @@ def home_page():
             'body': 'The Avengers movie was so cool!'
         }
     ]
+
+@app.route('/')
+@app.route('/index')
+def home_page():
+    
  
     return render_template('index.html',user=sampleuser,posts=posts)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',form=form,user=sampleuser)
