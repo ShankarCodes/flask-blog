faab36f212bb9f676a25bea5f1b9f7cc4a4d0b2d
Shankar <123456789987654321sr@gmail.com>
2020-01-11 19:54:02 +0530

    added flask login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from .app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer,primary_key=True)
@@ -8,6 +11,11 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username}, email={self.email}>'
